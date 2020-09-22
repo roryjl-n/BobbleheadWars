06b0a142e2fdf53d33cdf056a4c4566c75b17ad9
Rory <rory.lacayon@gmail.com>
2020-09-22 16:43:03 -0400

    Added Camera Movement and Added Gunplay

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //followTarget is what you want the camera to follow.
+    public GameObject followTarget;
+    // moveSpeed is the speed at which it should move.
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This code checks to see if there is a target available. If not, the camera doesn’t follow.
+        if (followTarget != null)
+        {
+            /*Next, Vector3.Lerp() is called to calculate the required position of the CameraMount.
+              Lerp() takes three parameters: A start position in 3D space, an end position in 3D
+              space, and a value between 0 and 1 that represents a point between the starting and
+              ending positions. Lerp() returns a point in 3D space between the start and end
+              positions that’s determined by the last value. */
+            transform.position = Vector3.Lerp(transform.position,
+            followTarget.transform.position, Time.deltaTime * moveSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // set the bulletPrefab to the bullet prefab
+    public GameObject bulletPrefab;
+    //  set the launchPosition to the position of the barrel of the Space Marine’s gun.
+    public Transform launchPosition;
+
+    void fireBullet()
+    {
+        /* 1 Instantiate() is a built-in method that creates a GameObject instance for a
+        particular prefab. In this case, this will create a bullet based on the bullet prefab.
+        Since Instantiate() returns a type of Object, the result must be cast into a GameObject. */
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        /* 2 The bullet’s position is set to the launcher’s position — you’ll set the launcher as
+        the barrel of the gun in just a moment. */
+        bullet.transform.position = launchPosition.position;
+        /* 3 Since the bullet has a Rigidbody attached to it, you can specify its velocity to make
+        the bullet move at a constant rate. Direction is determined by the transform of the
+        object to which this script is attached — you’ll soon attach it to the body of the
+        space marine, ensuring the bullet travels in same the direction as the marine faces. */
+        bullet.GetComponent<Rigidbody>().velocity =
+        transform.parent.forward * 100;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check with the Input Manager to see if the left mouse button is held down
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsInvoking("fireBullet"))
+            {
+                InvokeRepeating("fireBullet", 0f, 0.1f);
+            }
+        }
+        // gun stops firing once the user releases the mouse button
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("fireBullet");
+        }
+    }
+}
