c9ab4775ad545d72c9ad0dcb0d9a1a8f150d4b13
Rory <rory.lacayon@gmail.com>
2020-10-06 17:55:08 -0400

    C.6 - Animated Alien Prefab and Space Marine with Imported Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,6 +14,8 @@ public class PlayerController : MonoBehaviour
     //where we want the marine to stare.
     //we set value to zero at the start of the game.
     private Vector3 currentLookTarget = Vector3.zero;
+    //
+    public Animator bodyAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -59,10 +61,11 @@ public class PlayerController : MonoBehaviour
         0, Input.GetAxis("Vertical"));
         if (moveDirection == Vector3.zero)
         {
-            // TODO
+            bodyAnimator.SetBool("IsMoving", false);
         }
         else
         {
+            bodyAnimator.SetBool("IsMoving", true);
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
         }
 
